5ff90104a8f9243ec3527a9e039b71bf15c89491
Carlos Cervantes <alshira@gmail.com>
2022-12-07 11:02:47 -0600

    First commit

---
+++ cypress/e2e/visit.cy.js
@@ -0,0 +1,68 @@
+describe('Test SUITE #1', () => {
+    it("Visit URL", ()=> {
+        cy.visit("/textinput")
+    })
+    it("We log into the console", ()=> {
+        cy.log("This is the message in the console")
+    })
+    it("Visit URL2", ()=> {
+        cy.visit("/classattr")
+        cy.url().then((url)=>{
+            cy.log(`Printing our URL ${url}`)
+            expect(url).to.contains('')
+        })
+
+        //cy.get('.class1').expect().to.be.equal.click()
+    })
+   
+
+    it("Some methods yeld some info", ()=> {
+        cy.url().then((url)=>{
+            cy.log(`Printing our URL ${url}`)
+        })
+    })
+
+    //suportted on Cypress 12+ cross origin
+    it('navigates', () => {
+        //this is to avoid Error: Minified React error #418;
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+          })
+        cy.visit('https://www.cypress.io')
+        cy.visit('https://stackoverflow.com')
+        cy.origin('https://stackoverflow.com', () => {
+          cy.get('body') // yup all good
+        })
+      })
+
+      it("title validation", ()=> {
+          //this is to avoid Error: Minified React error #418;
+          Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+          })
+        //we read from file our fake location  
+        cy.fixture('mylocation.json').as('myLocation')
+        // we visit the site using our fake location
+        cy.get('@myLocation').then(myLocation => {
+            cy
+              .visit("https://www.liverpool.com.mx/", {
+                onBeforeLoad (win) {
+              cy
+                .stub(win.navigator.geolocation, 'getCurrentPosition')
+                .callsFake((cb) => {
+                       return cb(myLocation);
+                });
+                },
+            });
+          });
+        //we no longer use this
+        //cy.visit("https://www.liverpool.com.mx/")
+        cy.title().then((title)=>{
+            cy.log(title)
+        })
+    })
+})
